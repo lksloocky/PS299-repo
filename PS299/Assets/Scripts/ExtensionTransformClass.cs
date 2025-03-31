05184d2b50e63ed02d9a6baeae503f18a0b1462f
Gustavo Costa <lksloocky@gmail.com>
2025-04-01 02:48:08 +0900

    Polimento
    precisão de order in layer aumentada com o OffsetMarker

--- PS299/Assets/Scripts/ExtensionTransformClass.cs
+++ PS299/Assets/Scripts/ExtensionTransformClass.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public static class TransformExtensions
 {
-   public static int GetSortingOrder(this Transform transform) 
+   public static int GetSortingOrder(this Transform transform, float yOffset = 0) 
     {
-        return -(int)(transform.position.y * 100);
+        return -(int)(transform.position.y + yOffset * 100);
     }
 }
--- PS299/Assets/Scripts/YSortDinamico.cs
+++ PS299/Assets/Scripts/YSortDinamico.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 public class YSortDinamico : MonoBehaviour
 {
     private int _baseSortingOrder;
+    private float _YSortingOffset;
     [SerializeField] private SortableSprite[] _sortableSprites;
+    [SerializeField] private Transform _sortOffsetMarker;
+
+    void Start() 
+    {
+       _YSortingOffset = _sortOffsetMarker.position.y;
+    }
 
     void Update() 
     {
-        _baseSortingOrder = transform.GetSortingOrder();
+        _baseSortingOrder = transform.GetSortingOrder(_YSortingOffset);
         foreach (var sortableSprite in _sortableSprites)
         {
             sortableSprite.spriteRenderer.sortingOrder = _baseSortingOrder + sortableSprite.relativeOrder;
