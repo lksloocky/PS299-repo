0e3b3a82004a48944269a86de752d0542473e6bd
Gustavo Costa <lksloocky@gmail.com>
2025-04-07 05:06:37 +0900

    Agro em Patrulha
    Inimigos possuem detecção

--- PS299/Assets/Scripts/Patrulha.cs
+++ PS299/Assets/Scripts/Patrulha.cs
@@ -5,17 +5,30 @@ using System.Collections.Generic;
 
 public class Patrulha : MonoBehaviour
 {
+    //patrol
     [SerializeField] private Transform[] _waypoints;
     public float speed;
     public float _waitTime = 1f;
-
     private int _currentWaypointIndex = 0;
     private bool _isWaiting;
- 
+
+    //chase
+    private bool _isChasing;
+    public float chaseSpeed = 5f;
+    [SerializeField] private Transform _player;
+    [SerializeField] private CircleCollider2D _range;
+
+ void Start() 
+ {
+    _player = GameObject.FindWithTag("Player")?.transform;
+    _range = GetComponent<CircleCollider2D>();
+ }
     
     void Update()
     {
-        Patrol();
+        if(_isChasing) ChasePlayer();
+
+        else Patrol();
     }
 
     void Patrol() 
@@ -23,6 +36,7 @@ public class Patrulha : MonoBehaviour
         if(_isWaiting) return;
 
         Transform _targetWaypoint = _waypoints[_currentWaypointIndex];
+
          //move para o waypoint atual
         transform.position = Vector2.MoveTowards(transform.position, _targetWaypoint.position, speed * Time.deltaTime);
 
@@ -38,4 +52,26 @@ public class Patrulha : MonoBehaviour
 
         _currentWaypointIndex = (_currentWaypointIndex + 1) % _waypoints.Length;
     }
+
+    void ChasePlayer() 
+    {
+        if(_player == null) return;
+
+        transform.position = Vector2.MoveTowards(transform.position, _player.position, chaseSpeed * Time.deltaTime);
+
+        //lógica de ataque
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player")) _isChasing = true;
+    }
+
+
+     void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player")) _isChasing = false;
+    }
+
+
 }
